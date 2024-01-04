db659c1f13c0e89ed310652e577b5fd6e4b18296
pavelklindziuk <pavelklindziuk@gmail.com>
2024-01-04 17:49:28 +0300

    initial commit

--- src/main/java/com/klindziuk/shorty/controller/LinkController.java
+++ src/main/java/com/klindziuk/shorty/controller/LinkController.java
@@ -8,7 +8,9 @@ import com.klindziuk.shorty.repository.LinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.result.view.RedirectView;
 import reactor.core.publisher.Mono;
 
@@ -21,6 +23,8 @@ public class LinkController {
   public LinkController(LinkRepository linkRepository) {
     this.linkRepository = linkRepository;
   }
+
+
 
   @GetMapping("/{linkKey}")
   public Mono<RedirectView> redirect(@PathVariable String linkKey) {
--- src/main/java/com/klindziuk/shorty/controller/LinkRestController.java
+++ src/main/java/com/klindziuk/shorty/controller/LinkRestController.java
@@ -7,9 +7,14 @@ package com.klindziuk.shorty.controller;
 import com.klindziuk.shorty.model.LinkEntity;
 import com.klindziuk.shorty.repository.LinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.result.view.RedirectView;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class LinkRestController {
@@ -24,5 +29,15 @@ public class LinkRestController {
   @GetMapping("/links")
   public Flux<LinkEntity> getAllLinks() {
     return linkRepository.findAll();
+  }
+
+  @PostMapping("/add")
+  public Mono<LinkEntity> addLink(LinkEntity linkEntity) {
+    return linkRepository.save(linkEntity);
+  }
+
+  @DeleteMapping("/add")
+  public Mono<Void> addLink(Long linkId) {
+    return linkRepository.deleteById(linkId);
   }
 }
